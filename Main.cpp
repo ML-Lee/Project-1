62b20fc4cc986763d6fb272e233674e36b429035
AllanVang <Allankpvang@yahoo.com>
2025-11-30 17:19:08 -0900

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -24,27 +24,54 @@ using namespace std;
 
 int main()
 {
-
+    char quitGame = 'n'; //quits game if quit = 'y';
     displayMenu();//Displays Game Menu
     
-
+    while(quitGame != 'y')
     {
-        int menuChoice = validMenuOption(1, 3);
+        int menuChoice = validMenuOption(1, 4); //valid inputs from 1 -4
+        char playAgain;
 
         switch (menuChoice)
         {
-        case 1:
+        case 1: // PLAY THE GAME
             // Discards current input
             cin.ignore(numeric_limits <streamsize>::max(), '\n');
             startGame(); //Starts the game
+            
+            cout << "\nPlay again? (y/n)\n";
+            playAgain = validCharacter();
+            if (playAgain == 'y')
+            {
+                clearScreen();
+                displayMenu();
+            }
+            else if (playAgain == 'n')
+            {
+                quitGame = 'y';
+                cout << "Exiting Game\n";
+            }
+            
             break;
-        case 2:
+        case 2: //SEE SETTINGS
+            clearScreen();
             gameSettings();
+
+            displayMenu();//Displays Game Menu
             break;
-        case 3:
+        case 3: //SEE CREDITS
+            clearScreen();
             gameCredits();
+            displayMenu();
+            break;
+        case 4: //QUIT GAME
+            clearScreen();
+            quitGame = 'y';
+            cout << "Exiting Game\n";
             break;
         }
+
+    
     }
   
   
